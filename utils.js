8e66b8b36b6dfbc26e9277541c706832f152953f
eelof <elof.bigestans@gmail.com>
2023-12-04 15:43:19 +0100

    4

--- utils.js
+++ utils.js
@@ -4,4 +4,7 @@ module.exports = {
   lines: (filename) => {
     return fs.readFileSync("./input.txt", "utf-8").split("\n");
   },
+  range: (size, start = 0) => {
+    return [...Array(size).keys()].map((i) => i + start);
+  },
 };
